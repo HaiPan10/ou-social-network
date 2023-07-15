f45e694dfe456316840d238ee57c1b0a22e58593
Hai Phan <phanthanhhai3317@gmail.com>
2023-07-15 21:15:32 +0700

    [Hai] - server: config cloudinary and datasource

--- social_network/src/main/java/com/ou/configs/WebApplicationContextConfig.java
+++ social_network/src/main/java/com/ou/configs/WebApplicationContextConfig.java
@@ -1,20 +1,32 @@
 package com.ou.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {
-    "com.ou.controller"
+    "com.ou.controller",
+    "com.ou.repository",
+    "com.ou.service"
 })
+@PropertySource("classpath:configs.properties")
 public class WebApplicationContextConfig implements WebMvcConfigurer{
+    @Autowired
+    private Environment environment;
+
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
@@ -28,5 +40,15 @@ public class WebApplicationContextConfig implements WebMvcConfigurer{
         r.setSuffix(".jsp");
 
         return r;
+    }
+
+    @Bean
+    public Cloudinary getCloudinary(){
+        return new Cloudinary(ObjectUtils.asMap(
+            "cloud_name", environment.getProperty("CLOUDINARY_CLOUD_NAME"),
+            "api_key", environment.getProperty("CLOUDINARY_API_KEY"),
+            "api_secret", environment.getProperty("CLOUDINARY_API_SECRET"),
+            "secure", true
+        ));
     }
 }
