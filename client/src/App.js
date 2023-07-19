32ad4359bac7a41b9bc8890af4096686324a68fa
PlaIsMe <phongvulai96@gmail.com>
2023-07-19 13:42:35 +0700

    [Phong] delete nodes_module

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Login } from "./pages/login/Login";
 import { Register } from "./pages/register/Register";
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path:"/login",
+      element:<Login/>,
+    },
+    {
+      path:"/register",
+      element:<Register/>,
+    }
+  ])
+
   return (
     <div>
-      <Login/>
-      {/* <Register/> */}
+      <RouterProvider router={router}/>
     </div>
   );
 }
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -4,23 +4,27 @@ export const Register = () => {
   return (
     <div className="register">
       <div className="card">
-        <div className="left">
-          <h1>Lama Social.</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
-            alias totam numquam ipsa exercitationem dignissimos, error nam,
-            consequatur.
-          </p>
-          <span>Do you have an account?</span>
-        </div>
         <div className="right">
-          <h1>Register</h1>
+          <div className="right-top">
+            <h1>Mạng xã hội cựu sinh viên trường đại học Mở TP.HCM</h1>
+            <p>
+              Chức năng này giành riêng cho cựu sinh viên trường đại học Mở TP.HCM. Vui lòng cung cấp đủ thông tin cần thiết!
+            </p>
+          </div>
+          <div className="right-bottom">
+            <span>Bạn đã có tài khoản?</span>
+            <button>Đăng nhập ngay</button>
+          </div>
+        </div>
+        <div className="left">
+          <h1>Đăng ký</h1>
           <form>
-            <input type="text" placeholder="Username" />
+            <input type="text" placeholder="Họ" />
+            <input type="text" placeholder="Tên" />
             <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <input type="text" placeholder="Name" />
-            <button>Register</button>
+            <input type="password" placeholder="Mật khẩu" />
+            <input type="text" placeholder="Mã số sinh viên" />
+            <button>Đăng ký</button>
           </form>
         </div>
       </div>
