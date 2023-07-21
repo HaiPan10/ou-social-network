5ce326a29f52f64706c5953d49774b5966c3a601
Hai Phan <phanthanhhai3317@gmail.com>
2023-07-21 21:16:07 +0700

    [Hai] - update create db script

--- query.sql
+++ query.sql
@@ -7,3 +7,9 @@ INSERT INTO role(name) VALUES("TEACHER");
 INSERT INTO role(name) VALUES("ADMIN");
 
 SELECT * FROM account;
+SELECT * FROM user;
+SELECT * FROM role;
+
+INSERT INTO account(email, password, role_id) VALUES('hai123@gmail.com', '123456', 1);
+INSERT INTO user(id, first_name, last_name) VALUES (30, 'Hai', 'Phan Thanh');
+INSERT INTO user_student(id, student_identical) VALUES(30, '2051010083');
