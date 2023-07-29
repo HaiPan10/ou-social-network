070dbc432b1b19eeab4b27886f9782a70bc2e3db
PlaIsMe <phongvulai96@gmail.com>
2023-07-29 11:45:25 +0700

    [Phong] commit change for client side

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -9,7 +9,6 @@ import { EmailVerification } from '../pages/emailVerification/EmailVerification'
 import { AccountLocked } from '../pages/accountLocked/AccountLocked';
 import { AccountPending } from '../pages/accountPending/AccountPending';
 import { AccountRejected } from '../pages/accountRejected/AccountRejected';
-import { load } from 'react-cookies';
 
 export const Layout = () => {
     const [user, dispatch] = useContext(AuthContext)
@@ -17,23 +16,26 @@ export const Layout = () => {
 
     useEffect(() => {
         if (user !== null) {
-            console.log("GET STATUS")
-            console.log(load("access-token"))
             const getStatus = async () => {
                 try {
                     let res = await authAPI().get(endpoints['status'] + `/${user.id}`)
                     setStatus(res.data)
-                } catch (ex) {                    
+                } catch (ex) {  
+                    setStatus("ERROR")      
                 }  
             }
     
             getStatus()
         } else {
             console.log("LOGIN")
-            return <Navigate to="/login" />
+            setStatus("LOGIN")
         }
     }, [])
-    
+
+    if (user === null) {
+        return <Navigate to="/login" />
+    }
+
     let pageContent;
     if (status === "ACTIVE") {
         pageContent = (
@@ -47,13 +49,17 @@ export const Layout = () => {
             </div>
         )
     } else if (status === "EMAIL_VERIFICATION_PENDING") {
-    pageContent = <EmailVerification accountId={user.id}/>
+        pageContent = <EmailVerification accountId={user.id}/>
     } else if (status === "LOCKED") {
-    pageContent = <AccountLocked/>
+        pageContent = <AccountLocked/>
     } else if (status === "AUTHENTICATION_PENDING") {
-    pageContent = <AccountPending/>
+        pageContent = <AccountPending/>
     } else if (status === "REJECT") {
-    pageContent = <AccountRejected/>
+        pageContent = <AccountRejected/>
+    } else if (status === "ERROR") {
+        pageContent = <NavBar/>
+    } else if (status === "LOGIN") {
+        return <Navigate to="/login" />
     }
 
     return (
--- client/src/configs/Api.jsx
+++ client/src/configs/Api.jsx
@@ -8,12 +8,25 @@ export const endpoints = {
     "status": "/status"
 }
 
-export const authAPI = () => axios.create({
-    baseURL: "http://127.0.0.1:8080/social_network/api",
-    headers: {
-        "Authorization": `Bearer ${ load("access-token") }`
-    }
-})
+// export const authAPI = () => axios.create({
+//     baseURL: "http://127.0.0.1:8080/social_network/api",
+//     headers: {
+//         "Authorization": `Bearer ${ load("access-token") }`
+//     }
+// })
+
+export const authAPI = () => {
+    const accessToken = load("access-token");
+    console.log("Access Token:", accessToken);
+  
+    return axios.create({
+      baseURL: "http://127.0.0.1:8080/social_network/api",
+      headers: {
+        "Authorization": `Bearer ${accessToken}`
+      }
+    });
+  };
+  
 
 export default axios.create({
     baseURL: 'http://127.0.0.1:8080/social_network/api',
