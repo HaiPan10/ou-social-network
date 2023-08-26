72fec30aaf763a96243c9104667fdb8d6ce4daee
HaiPan10 <phanthanhhai3317@gmail.com>
2023-08-26 19:28:14 +0700

    [Hai] - merge

--- social_network/src/main/java/com/ou/api/ApiAdminController.java
+++ social_network/src/main/java/com/ou/api/ApiAdminController.java
@@ -2,8 +2,6 @@ package com.ou.api;
 
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
